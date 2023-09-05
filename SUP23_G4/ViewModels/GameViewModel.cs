b33efe045859bc21ab90793c05ce263f11bb948f
M-Brixvi <dreadalizer@gmail.com>
2023-09-05 13:25:33 +0200

    Method for tossing dice

--- SUP23_G4/ViewModels/GameViewModel.cs
+++ SUP23_G4/ViewModels/GameViewModel.cs
@@ -11,15 +11,28 @@ namespace SUP23_G4.ViewModels
 {
     public class GameViewModel : BaseViewModel
     {
-        public ObservableCollection<Die>? Dice { get; private set; }
+        
+        public int DieOne { get; private set; }
+        public int DieTwo { get; private set; }
 
         public void DiceToss()
         {
             Random r = new Random();
-            Die die = new Die
+            
+            for (int i = 0; i < 2; i++)
             {
-                DieValue = r.Next(1, 7)
-            };
+                Die die = new Die();
+                if (i == 0)
+                {
+                    die.DieValue = r.Next(1, 7);
+                    DieOne = die.DieValue;
+                }
+                else if (i == 1)
+                {
+                    die.DieValue = r.Next(1, 7);
+                    DieTwo = die.DieValue;
+                }
+            }          
         }
     }
 }
