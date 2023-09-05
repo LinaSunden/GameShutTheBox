05fc42d33ba8817e0868a41bdf545feca7dfc210
Frida Saari <frsa1402@student.miun.se>
2023-09-05 09:32:44 +0200

    Tile

---
+++ SUP23_G4/Views/GameTiles/Tile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUP23_G4.Views.GameTiles
+{
+    internal class Tile 
+    {
+    }
+}
