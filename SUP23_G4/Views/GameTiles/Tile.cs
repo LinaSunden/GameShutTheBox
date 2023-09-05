2af48dd6daec849c90205199e455b58862d08957
LinaSunden <sundenlina@gmail.com>
2023-09-05 09:34:34 +0200

    Tile Ärver UserControl

--- SUP23_G4/Views/GameTiles/Tile.cs
+++ SUP23_G4/Views/GameTiles/Tile.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace SUP23_G4.Views.GameTiles
 {
-    internal class Tile 
+    internal class Tile : UserControl
+
     {
     }
 }
