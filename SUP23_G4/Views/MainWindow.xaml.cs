3d817437d818eb85a59d446d7d58faeabbd6bfd1
M-Brixvi <dreadalizer@gmail.com>
2023-09-05 10:35:01 +0200

    Connected Views with ViewModels

--- SUP23_G4/Views/MainWindow.xaml.cs
+++ SUP23_G4/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SUP23_G4.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace SUP23_G4
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainViewModel();
         }
     }
 }
