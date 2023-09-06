32a9d6d7f52a4c5fa02b1c68b92cf194c5857d30
Frida Saari <frsa1402@student.miun.se>
2023-09-06 11:06:10 +0200

    GameViewRules#1

--- SUP23_G4/ViewModels/StartViewModel.cs
+++ SUP23_G4/ViewModels/StartViewModel.cs
@@ -15,14 +15,18 @@ namespace SUP23_G4.ViewModels
         public StartViewModel(MainViewModel mainViewModel)
         {
             StartGameCommand = new RelayCommand(x => SwitchToGameView());
+            GameRulesCommand = new RelayCommand(x => SwitchToGameRules());
             this._mainViewModel = mainViewModel;
+           
 
         }
+  
 
         #endregion
 
         #region Egenskaper
         public ICommand StartGameCommand { get; set; }
+        public ICommand GameRulesCommand { get; set; }
 
         #endregion
 
@@ -44,6 +48,10 @@ namespace SUP23_G4.ViewModels
         {
             _mainViewModel.CurrentViewModel = new GameViewModel();
 
+    }
+    protected void SwitchToGameRules()
+        {
+            _mainViewModel.CurrentViewModel = new GameRulesModel();
         }
 
         #endregion
