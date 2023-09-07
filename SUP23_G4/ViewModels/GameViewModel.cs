bf0ab0bb364d6fe8960908a44b285a194d5fc275
LinaSunden <sundenlina@gmail.com>
2023-09-07 04:58:00 +0200

    Skapat en mapp Components. Lagt in Die som en UserControl. Tagit bort den tidigare Die klassen.

--- SUP23_G4/ViewModels/GameViewModel.cs
+++ SUP23_G4/ViewModels/GameViewModel.cs
@@ -1,5 +1,5 @@
 ﻿using SUP23_G4.ViewModels.Base;
-using SUP23_G4.Views.Dice;
+using SUP23_G4.Views.Components;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
---
+++ SUP23_G4/Views/Components/Die.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SUP23_G4.Views.Components
+{
+    /// <summary>
+    /// Interaction logic for Die.xaml
+    /// </summary>
+    public partial class Die : UserControl
+    {
+        public Die()
+        {
+            InitializeComponent();
+        }
+
+
+        public int DieValue { get; set; }
+
+    }
+}
