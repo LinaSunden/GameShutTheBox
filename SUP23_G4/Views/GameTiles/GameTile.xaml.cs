0b600dbf2fb34293a2e0c8b8292ee467e80e2d12
Frida Östberg <117045810+Fros2200@users.noreply.github.com>
2023-09-11 13:16:47 +0200

    Nu funkar kopplingen till available/notAvailable tiles

--- SUP23_G4/Views/GameTiles/GameTile.xaml.cs
+++ SUP23_G4/Views/GameTiles/GameTile.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SUP23_G4.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,6 +26,7 @@ namespace SUP23_G4.Views.GameTiles
             InitializeComponent();
         }
 
-        
+
+
     }
 }
