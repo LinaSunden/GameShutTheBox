9916c229785d68e0f3e75f23bf527c1a4ba89b07
LinaSunden <sundenlina@gmail.com>
2023-09-15 13:51:48 +0200

    Ny klass skapad för språk

---
+++ SUP23_G4/Languages/Language.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUP23_G4.Languages
+{
+    public class Language
+    {
+
+    }
+}
